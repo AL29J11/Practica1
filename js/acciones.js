78ef426928541e63d340d39651060725333e088a
Alex Ramos <alexithorp13@gmail.com>
2013-03-20 08:00:30 -0600

    Version 1.0 (20/03/13)

---
+++ js/acciones.js
@@ -0,0 +1,30 @@
+// JavaScript Document
+
+$(document).ready(function(e) {
+  //alert("Listo")
+    document.addEventListener("deviceready",function(){
+	   $('.btn').click(function(){
+		   var boton=(($(this).attr('class')).split(' '))[1];
+		   //alert(boton)
+		   switch(boton)
+		   {
+		   case 'b1':
+		    navigator.notification.beep(1);
+		   break;
+		   case 'b2':
+		    navigator.notification.beep(2);
+		   break;
+		   case 'b3':
+		    navigator.notification.beep(5);
+		   break;
+		   case 'v1':
+		    navigator.notification.vibrate(500);
+		   break;
+		   case 'v2':
+		    navigator.notification.vibrate(1000);
+			navigator.notification.vibrate(2000);
+		   break;		   		   		   		   
+		   }
+	   });
+   },false);
+});
